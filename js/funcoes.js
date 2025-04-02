a02428cba4df77342f8941673517b43b47765317
Jesse Silva <jesse1eliseu@gmail.com>
2025-04-02 19:21:11 -0300

    Adiciona nova página HTML com elementos bootstrap

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,7 +1,11 @@
-document.getElementById('hello-world').addEventListener('click', function(){
-    alert('Hello World');
-});
+// document.getElementById('hello-world').addEventListener('click', function(){
+//     alert('Hello World');
+// });
 
 function soma (n1,n2){
     alert(n1+n2);
 }
+
+// $(document).ready(function(){
+//     alert('Hello World');
+// });
